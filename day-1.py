dfa51b6f3f480e55615fc10bf5d39b30aa4e33e9
Dheenadhayalan <amigodheena@gmail.com>
2021-12-01 21:26:09 +0530

    Create day-1.py

---
+++ day-1.py
@@ -0,0 +1,12 @@
+#Print lenth of the charactor
+#print(len(input("Enter your name: ")))
+
+#variable
+#name = input("Enter your name: ")
+#print("Hello " + name)
+
+#Generate Band name
+print("Welcome to the band name generator")
+city = input("Your native place?\n")
+pet  = input("Your pet name?\n")
+print("Your band name is " + city + " " + pet)
